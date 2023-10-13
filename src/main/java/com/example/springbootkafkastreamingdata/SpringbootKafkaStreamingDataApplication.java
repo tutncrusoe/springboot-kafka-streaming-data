c3593fc2e1a2d23f1d78dac87b72cc55547681bc
tutncrusoe <huydinhanhle@gmail.com>
2023-10-13 15:44:26 +0700

    create: kafka-streaming-data

---
+++ src/main/java/com/example/springbootkafkastreamingdata/SpringbootKafkaStreamingDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkastreamingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaStreamingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaStreamingDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootkafkastreamingdata/SpringbootKafkaStreamingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkastreamingdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaStreamingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
