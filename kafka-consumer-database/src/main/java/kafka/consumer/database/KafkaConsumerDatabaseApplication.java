5b6e0d7433abddc63633ce428e521eb5a4dccac3
tutncrusoe <huydinhanhle@gmail.com>
2023-10-13 16:43:29 +0700

    create: kafka-consumer-database

---
+++ kafka-consumer-database/src/main/java/kafka/consumer/database/KafkaConsumerDatabaseApplication.java
@@ -0,0 +1,14 @@
+package kafka.consumer.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerDatabaseApplication.class, args);
+    }
+
+
+}
