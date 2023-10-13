3c756a485a593dd1101b8326edfef2238e367bf0
tutncrusoe <huydinhanhle@gmail.com>
2023-10-13 15:56:21 +0700

    create: kafka-producer-wikimedia

---
+++ kafka-producer-wikimedia/src/main/java/kafka/producer/wikimedia/KafkaProducerWikimediaApplication.java
@@ -0,0 +1,13 @@
+package kafka.producer.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerWikimediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerWikimediaApplication.class, args);
+	}
+
+}
